4ba1c231f9c4714c32fcb29c81a0000f9febeee2
digitalmaxime <maxime.rheault@gmail.com>
2025-11-03 07:24:09 -0500

    kiss the wife

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -15,7 +15,8 @@ var sampleTodos = new Todo[]
     new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
     new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
     new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2))),
+    new(6, "Kiss the wife", DateOnly.FromDateTime(DateTime.Now.AddDays(3)))
 };
 
 var todosApi = app.MapGroup("/todos");
